5a6675b9691886ad9350f94f986453dc62d3c778
Hadar Sarig <hadar.sarig@post.runi.ac.il>
2025-11-21 12:56:23 +0200

    anagram2

--- Anagram.java
+++ Anagram.java
@@ -79,7 +79,7 @@ public class Anagram {
         
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
-            if ((c >= 'a' && c <= 'z') || c == ' ') {
+            if (c >= 'a' && c <= 'z') {
                 processedString = processedString + c;
             }
         }
